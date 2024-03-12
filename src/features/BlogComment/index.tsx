e64db2c1145befd124e6e0a81bbd90463779ad07
linxiaowu66 <linguang661990@126.com>
2024-03-12 20:44:17 +0800

    feat(评论): 使用新的评论系统

---
+++ src/features/BlogComment/index.tsx
@@ -0,0 +1,23 @@
+"use client";
+import Giscus from "@giscus/react";
+const BlogComment = () => {
+  return (
+    <Giscus
+      id="comments"
+      repo="linxiaowu66/doumi-blog-comments"
+      repoId="MDEwOlJlcG9zaXRvcnkyMjMwNTk4MzQ="
+      category="Announcements"
+      categoryId="DIC_kwDODUufes4Cd6ZO"
+      mapping="og:title"
+      term="Welcome to @giscus/react component!"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme="light"
+      lang="en"
+      loading="lazy"
+    />
+  );
+};
+
+export default BlogComment;
